b485e19536bee3c68a422b54372af7ec0ad1f4a9
MarwaEzzatMo <marwaezzat201@gmail.com>
2022-05-16 12:23:13 +0200

    day 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :users
   root "articles#index"
-  resources :articles
+  resources :articles do
+    resources :comments
+  end  
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
